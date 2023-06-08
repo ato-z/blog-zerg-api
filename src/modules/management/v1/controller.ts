b14a7b2d22dc048e5e3510fdd2bfd16b0f057dd6
atoz <523333404@qq.com>
2023-06-08 23:06:04 +0800

    🚲 颁发sign token

--- src/modules/management/v1/controller.ts
+++ src/modules/management/v1/controller.ts
@@ -1,26 +1,36 @@
-import { type Context, POST, Group } from '@atoz/router';
+import { type Context, POST, GET, Group } from '@atoz/router';
 import { resultSuccess } from '@/helper/functions';
 import { DtoLogin } from './dto/Login';
-import { Validate } from '@/validate';
+import { ServiceManagement } from '@/service/Management';
+import { ServiceToken } from '@/service/Token';
 
 @Group('/management')
 export class Management {
   @POST('login') async login(ctx: Context) {
-    const post = new DtoLogin(await ctx.getBody());
-    await new Validate(post, [
-      {
-        name: 'user',
-        rule: {
-          length: ['用户名$msg', 4, 12],
-        },
-      },
-      {
-        name: 'password',
-        rule: {
-          required: ['密码$msg'],
-        },
-      },
-    ]).check();
-    return resultSuccess('ok', post);
+    const post = await ctx.getBody<DtoLogin>();
+    const dto = new DtoLogin(post);
+
+    const service = new ServiceManagement(ctx.env);
+    const sign = await service.touchSign(dto);
+    return resultSuccess('ok', { sign });
+  }
+
+  @GET('token') async getToken(ctx: Context) {
+    const { params } = ctx;
+    const { sign } = params as { sign: string };
+    const serviceManagement = new ServiceManagement(ctx.env);
+    const management = await serviceManagement.decodeSign(sign);
+    const token = ServiceToken.instance<ServiceToken>(
+      ctx.env,
+    ).createByManagement(management.metadata, management.value);
+    return resultSuccess('ok', { token });
+  }
+
+  @GET('data') async getData(ctx: Context) {
+    const management = await ServiceToken.instance<ServiceToken>(
+      ctx.env,
+    ).getManagementByCtx(ctx);
+
+    return resultSuccess('ok', management);
   }
 }
--- src/modules/management/v1/services/management.ts
+++
@@ -1,3 +0,0 @@
-export class ServiceManagement {
-  constructor(protected db: KVNamespace) {}
-}
